cf3a73272c93662a0c815261a88c8203319c3694
Lucas Felipe <lkzeron@gmail.com>
2023-10-09 17:36:48 -0300

    feat: introdução do swagger e documentar com hapi

---
+++ Swagger/hapi-swagger/api.js
@@ -0,0 +1,51 @@
+// npm i vision inert haí-swagger
+
+const Hapi = require('hapi')
+const Context = require('./src/db/strategies/base/contextStrategy')
+const MongoDB = require('./src/db/strategies/mongodb/mongoDbStrategy')
+const HeroSchema = require('./src/db/strategies/mongodb/schemas/heroSchema')
+const HeroRoutes = require('./src/routes/heroRoutes')
+
+//swagger 
+
+const HapiSwagger = require('hapi-swagger')
+const Vision = require('vision');
+const Inert = require('inert')
+const app = new Hapi.Server({
+    port: 4000
+})
+
+function mapRoutes(instance, methods) {
+    return methods.map(method => instance[method]())
+}
+
+async function main() {
+    const connection = MongoDB.connect()
+    const mongoDb = new Context(new MongoDB(connection, HeroSchema))
+   const swaggerOptions = {
+    info:{
+        titile:'Api Herois  - Teste CrusoNodeBr',
+        version: 'v1.0'
+    },
+    lang:'pt'
+   }
+    await app.register([
+        Vision,
+        inert,
+        {
+        plugin: HapiSwagger,
+        options :swaggerOptions,
+        }
+    ])
+
+
+    app.route([
+        ...mapRoutes(new HeroRoutes(mongoDb), HeroRoutes.methods())
+    ])
+
+    await app.start()
+    console.log('server running at', app.info.port)
+
+    return app;
+}
+module.exports = main()
---
+++ Swagger/hapi-swagger/swagger.md
@@ -0,0 +1,3 @@
+<h1>Swagger comando inicial</h1>
+
+<li> Npm i vision inert hapi-swagger
